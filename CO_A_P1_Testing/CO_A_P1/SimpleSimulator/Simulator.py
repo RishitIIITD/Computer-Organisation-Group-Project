d531e1a6159b9bfced6f76045358a29e6c63e22f
RishitIIITD <132936001+RishitIIITD@users.noreply.github.com>
2023-06-06 23:46:10 +0530

    final update(hopefully)

--- CO_A_P1_Testing/CO_A_P1/SimpleSimulator/Simulator.py
+++ CO_A_P1_Testing/CO_A_P1/SimpleSimulator/Simulator.py
@@ -44,16 +44,14 @@ for i in range(128):
 lines=[]
 memIndex=0
 
-with open("input.txt",'r') as reader:
-    for line in reader:
-        line=line.strip()
-        #sys.stdout.write("\nLine:\n"+line)
-        
-        if line==" " or line=="\n" or line=="":
-            continue
-        memLines[memIndex]=line
-        memIndex+=1
-        lines.append(line)
+for line in file_input.readlines():
+    line=line.strip()
+
+    if line==" " or line=="\n" or line=="":
+        continue
+    memLines[memIndex]=line
+    memIndex+=1
+    lines.append(line)
 
 isFlagNotSet=True
 isJump=False
@@ -65,15 +63,15 @@ prgCntUpd=0
 while(prgCnt<len(lines)):
     line=lines[prgCnt]
     opcode=line[:5]
-#typeA Register
+
     if opcode in ["00000","00001","00110","01010","01011","01100"]:
         reg1=line[7:10]
         reg2=line[10:13]
         reg3=line[13:]
-    #addition
+
         if opcode == "00000":
             registers[reg1]=overflowflag(registers[reg2]+registers[reg3])
-    #subtraction
+
         elif opcode == "00001":
             val=registers[reg2]-registers[reg3]
             if val>=0:
@@ -82,35 +80,35 @@ while(prgCnt<len(lines)):
                 registers["111"]="0"*12+"1000"
                 registers[reg1]=0
                 isFlagNotSet=False
-    #multiplication
+
         elif opcode =="00110":
             registers[reg1]=overflowflag(registers[reg2]*registers[reg3])
-    #XOR operation
+
         elif opcode =="01010":
             registers[reg1]=registers[reg2]^registers[reg3]
-    #OR_operation
+
         elif opcode =="01011":
             registers[reg1]=registers[reg2]|registers[reg3]
-    #AND_operation
+
         elif opcode =="01100":
             registers[reg1]=registers[reg2]&registers[reg3]
 
-    #type B Regs
+
     elif opcode in ["00010","01000","01001"]:
         reg1=line[6:9]
         imm=BinaryToDecimal(line[9:])
         
-        #move immediate
+
         if opcode == "00010":
             registers[reg1]=imm
-        #right shift operation
+
         elif opcode == "01000":
             val=registers[reg1]>>imm
             if val<0:
                 registers[reg1]=0
             else:
                 registers[reg1]=val
-        #left shift operation
+
         elif opcode == "01001":
             lval=registers[reg1]<<imm
             if (lval>=65536):
@@ -118,14 +116,14 @@ while(prgCnt<len(lines)):
             else:
                 registers[reg1]=lval
 
-    #type C Registers
+
     elif opcode in ["00011","00111","01101","01110"]:
         reg1=line[10:13]
         reg2=line[13:]
-        #move register
+
         if opcode == "00011":
             registers[reg1]=registers[reg2]
-        #divide 
+
         elif opcode == "00111":
             if registers[reg2] == 0:
                 registers["000"]=0
@@ -135,7 +133,7 @@ while(prgCnt<len(lines)):
             else:
                 registers["000"]=registers[reg1]//registers[reg2]
                 registers["001"]=registers[reg1]%registers[reg2]
-        #Invert NOT operations
+
         elif opcode == "01101":
             lval=DecimalToBinary(registers[reg2])
             s=''
@@ -145,7 +143,7 @@ while(prgCnt<len(lines)):
                 else:
                     s=s+'0'
             registers[reg1]=BinaryToDecimal(s)
-        #compare
+
         elif opcode == "01110":
             isFlagNotSet=False
             val= int(registers[reg1])-int(registers[reg2])
@@ -156,39 +154,39 @@ while(prgCnt<len(lines)):
             elif val <0 :
                 registers["111"]="0"*12+"0100"
 
-    #type D Registers
+
     elif opcode in ["00100","00101"]:
         reg1=line[6:9]
         memAddrs=BinaryToDecimal(line[9:])
-        #load
+
         if opcode == "00100":
             registers[reg1]=BinaryToDecimal(memLines[memAddrs])
-        #store
+
         elif opcode == "00101":
             memLines[memAddrs]=DecimalToBinary(registers[reg1])
-    #type E Registers
+
     elif opcode in ["01111","11100","11101","11111"]:
         memAddrs=BinaryToDecimal(line[9:])
-        #unconditional jump
+
         if opcode == "01111":
             prgCntUpd=memAddrs
             isJump=True
-        #jump if less than
+
         elif opcode == "11100":
             if registers["111"]=="0"*12+"0100":
                 prgCntUpd=memAddrs
                 isJump=True
-        #jump if Greater Than
+
         elif opcode =="11101":
             if registers["111"]=="0"*12+"0010":
                 prgCntUpd=memAddrs
                 isJump=True
-        #jump if Equal
+
         elif opcode == "11111":
             if registers["111"]=="0"*12+"0001":
                 prgCntUpd=memAddrs
                 isJump=True
-    #type F Regs halt
+
     elif opcode in ["11010"]:
         isHalt=True
 
@@ -216,7 +214,7 @@ while(prgCnt<len(lines)):
     
     prgCnt+=1
     
-    if isFlagNotSet==False:  # reset back with 0
+    if isFlagNotSet==False:
         registers['111']='0'*16
     
     isFlagNotSet=True
